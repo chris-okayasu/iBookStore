7d521cce767451dbd1571001009bb4d0640b4a66
Chris <san.kyu@icloud.com>
2024-10-28 11:46:37 -0600

    new: First Commit

---
+++ iBookStore/iBookStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBookStoreApp.swift
+//  iBookStore
+//
+//  Created by chris on 2024/10/28.
+//
+
+import SwiftUI
+
+@main
+struct iBookStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
